545004466fdf43c48dce4ea9ba0d08ec895d9987
Farjad Habib <habib.farjad@gmail.com>
2019-08-28 19:25:07 -0700

    Javascript Basics

---
+++ 4-Javascript/JavascriptBasic.js
@@ -0,0 +1,23 @@
+//alert("Hey! javascript file is linked properly.");
+
+var _mineString123 ="MyString";
+var _mineString124 ="YourString";
+var _mineString125 = 20;
+var _mineString126 = 10;
+var sumString=_mineString125+_mineString126;
+var isThisMyName=true;
+var combinedString = _mineString123+" "+_mineString124+"-"+sumString+"_"+isThisMyName;
+
+//alert(combinedString);
+
+console.log(combinedString);
+
+var showName = prompt("name you favorite tv show.");
+
+if(showName=="gold"){
+    alert("hurray! i found your tv show;");
+}else if(showName=="silver"){
+    alert("is is a fine show.");
+}else{
+    alert("i don't know that show.");
+}
