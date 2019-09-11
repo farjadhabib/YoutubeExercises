2c716e9a9eddee5718e783f47752f07c206c706b
Farjad Habib <habib.farjad@gmail.com>
2019-09-10 21:56:32 -0700

    7th lecture

--- 4-Javascript/JavascriptBasic.js
+++ 4-Javascript/JavascriptBasic.js
@@ -1,6 +1,6 @@
 //alert("Hey! javascript file is linked properly.");
 
-var _mineString123 ="MyString";
+/*var _mineString123 ="MyString";
 var _mineString124 ="YourString";
 var _mineString125 = 20;
 var _mineString126 = 10;
@@ -20,4 +20,33 @@ if(showName=="gold"){
     alert("is is a fine show.");
 }else{
     alert("i don't know that show.");
+}*/
+
+var myArray = ["Red", "Blue", "Green"];
+
+/*alert(myArray.length);
+alert(myArray[0]);
+
+for(var i =0;i<=10;i++){
+    console.log(i);
+}
+
+var x=20;
+while(x>10){
+    console.log(x);
+    x--;
+}*/
+
+for(var i=0;i<myArray.length;i++){
+    console.log(myArray[i]);
+}
+
+
+function buttonClicked(whichButton){
+    console.log(whichButton+" is clicked.");
+    console.log(addTwoNumbers(10,5));
+}
+
+function addTwoNumbers(number1, number2){
+    return number1+number2;
 }
